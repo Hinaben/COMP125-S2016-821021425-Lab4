5ab479058cd7186903239486d12ef1f71c9f652a
Hinaben <henapatel21@hotmail.com>
2016-07-06 18:35:53 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# COMP125-Lab3
+
+## A project to demonstrate working with the Document Object Model (DOM) and DHML for COMP125 @ Centennial
---
+++ Scripts/app.js
@@ -0,0 +1,31 @@
+/**
+ * FileName: app.js
+ * 
+ * @author Hina Patel
+ * @date June 7, 2016
+ * 
+ * StudentID: 821021425
+ * website: http://comp125-s2016-lab3.azurewebsites.net
+ * @description This file is the main javascript file for the web site
+ */
+
+// IIFE - Immediately Invoked Function Expression
+(function(){
+    "use strict";
+    
+var User = {
+ID: "1",
+Name:"admin ",
+Email:"admin@example.com",
+Password: "123456"
+};
+
+function init() {
+    console.log("Application started ")
+}
+
+
+
+    
+    
+})();
